d8bfcf9991ec53b7784b00cad3cd54aa2be6bb5e
Adarsh Das <adarsh.191605@ncit.edu.np>
2023-12-26 23:19:49 +0545

    displayed products

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((data) => setProducts(data.data.products))
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/carts/2")
+      .then((data) => console.log(data.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
 }
 
-export default App
+function Product(props) {
+  const { id, title, description, price, brand, images } = props.product;
+  return (
+    <div>
+      <p>id:{id}</p>
+      <p>title:{title}</p>
+      <p>description:{description}</p>
+      <p>price:{price}</p>
+      <p>brand:{brand}</p>
+      <img src={images[0]} alt={title} />
+    </div>
+  );
+}
+
+function Cart() {}
+
+export default App;
